b7cb0e867321a4cd8c47f5815e965c4f30899036
srilekaam <srilekaam@gmail.com>
2025-06-12 17:41:21 +0530

    Update great.py

--- great.py
+++ great.py
@@ -6,3 +6,8 @@ elif num2 >= num1 and num2 >= num3:
   print(num2)
 else:
   print(num3)
+
+num1, num2, num3 = 10 , 30 , 20
+max = num1 if num1>num2 else num2
+max = num3 if num3>max else max
+print(max)
